cbaa17ab4ded576aa79bbe4fea75482ecac3c1a8
garance-lat <151042780+garance-lat@users.noreply.github.com>
2024-03-06 12:38:17 +0100

    Update RF_thun.py

--- partie II/RF_thun.py
+++ partie II/RF_thun.py
@@ -8,6 +8,8 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
+# **gère les missing values seul RF ? en tout cas l'accuracy augment de 0,01 en ne remplacant pas les MV par moyenne comme SVM
+
 # Charger le jeu de données
 path = "/home/onyxia/work/projet-python/"
 file_name1 = "dataset_complet_part_1.csv"
@@ -22,6 +24,7 @@ df['grav'] = df['grav'].replace({1: 0, 2: 0, 3: 1, 4: 1})
 
 # Suppression des colonnes non nécessaires
 df = df.drop(columns=['Num_Acc', 'an', 'adr', 'lat', 'long', 'lartpc', 'larrout', 'com', 'gps', 'voie', 'dep', 'v2', 'pr', 'pr1'])
+# **comment a été choisi ces var ?
 df['hrmn'] = df['hrmn'].str.split(':').str[0]
 
 # Enregistrement des données préparées dans un fichier CSV
@@ -36,15 +39,24 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
+# **pas besoin de normaliser mais ca peut accélérer la cv ?
 
 # Définir les hyperparamètres à tester
 param_grid = {
-    'n_estimators': [50, 100, 150],
-    'max_depth': [None, 10, 20],
+    'n_estimators': [50, 100, 150], # nb d'arbres (+ élevé, + performant, + temps de calcul
+    'max_depth': [None, 10, 20], # profondeur max de chaque arbre (+ profond + détails dans les données + risque d'overfitting)
     'min_samples_split': [2, 5, 10],
     'min_samples_leaf': [1, 2, 4],
     'max_features': ['auto', 'sqrt']
 }
+
+min_samples_split : 
+
+min_samples_leaf : Le nombre minimum d'échantillons requis pour être à un nœud feuille (terminal). Comme avec min_samples_split, une valeur plus élevée peut aider à régulariser le modèle.
+
+max_features : Le nombre maximum de fonctionnalités à considérer pour chaque division d'un nœud. Les options typiques incluent 'auto' (équivalent à sqrt(n_features)) et 'sqrt' (également connu sous le nom de 'auto'), qui recherchent les meilleures fonctionnalités parmi toutes les fonctionnalités disponibles.
+
+En utilisant une recherche par grille (Grid Search), vous pouvez spécifier différentes combinaisons de ces hyperparamètres, et le Grid Search essaiera toutes les combinaisons possibles pour trouver celle qui maximise les performances du modèle, généralement mesurées par une métrique comme la précision, le F-score, etc.
 
 # Créer un classifieur Random Forest
 rf_classifier = RandomForestClassifier(random_state=42)
